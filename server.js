43a7d497c618d6659e7d20b65cb735fb6005749e
Dawid <dawidmuraszewski@web.de>
2022-06-08 14:21:37 +0200

    zusammen gemergt

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 import express from "express";
 import "dotenv/config";
 import mongoose from "mongoose";
-import connnectMongoose from "./util/connnectMongoose.js"
-import fehler from "./routes/fehler.js"
-import indexRouter from "./routes/index.js"
+import connectMongoose from "./util/mongoose_connect.js";
+import fehler from "./routes/fehler.js";
+import indexRouter from "./routes/index.js";
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 
 const PORT = 3000;
 
-if (await connnectMongoose()) {
-app.listen(PORT, (err) => {
-if (err) console.error(err);
-console.log(`listening to Port ${PORT}`);
-});
-} 
+if (await connectMongoose()) {
+	app.listen(PORT, (err) => {
+		if (err) console.error(err);
+		console.log(`listening to Port ${PORT}`);
+	});
+}
 
-app.use("/", indexRouter)
+app.use("/", indexRouter);
 
-app.use (fehler)
+app.use(fehler);
